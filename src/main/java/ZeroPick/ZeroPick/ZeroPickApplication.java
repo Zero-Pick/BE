3db3bfe80c5e87e508074461eeb87921b3983175
Suhun0331 <rlatngns6046@naver.com>
2024-10-22 20:21:34 +0900

    ReadmeTest

---
+++ src/main/java/ZeroPick/ZeroPick/ZeroPickApplication.java
@@ -0,0 +1,13 @@
+package ZeroPick.ZeroPick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ZeroPick/ZeroPick/ZeroPickApplicationTests.java
@@ -0,0 +1,13 @@
+package ZeroPick.ZeroPick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
