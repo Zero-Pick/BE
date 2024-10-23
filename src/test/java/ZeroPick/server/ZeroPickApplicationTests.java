fedbb4e05ba5908cf0e36ee7bbd3b47fa31cb830
Suhun0331 <rlatngns6046@naver.com>
2024-10-23 12:14:45 +0900

    CICD test

---
+++ src/test/java/ZeroPick/server/ZeroPickApplicationTests.java
@@ -0,0 +1,13 @@
+package ZeroPick.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ZeroPickApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // 테스트 코드
+    }
+}
