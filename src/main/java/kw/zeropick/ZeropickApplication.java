923dbde1f1af9a7e68122d9e879a91cb844fe11f
Suhun0331 <rlatngns6046@naver.com>
2024-11-07 14:22:56 +0900

    초기세팅

---
+++ src/main/java/kw/zeropick/ZeropickApplication.java
@@ -0,0 +1,13 @@
+package kw.zeropick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeropickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeropickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kw/zeropick/ZeropickApplicationTests.java
@@ -0,0 +1,13 @@
+package kw.zeropick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeropickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
