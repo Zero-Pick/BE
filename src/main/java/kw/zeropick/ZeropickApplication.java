b59c2512faa4beafc14830f626aad3df3a4625b1
Suhun0331 <rlatngns6046@naver.com>
2024-11-14 18:35:59 +0900

    Feat : Add configuration3

--- src/main/java/kw/zeropick/ZeropickApplication.java
+++ src/main/java/kw/zeropick/ZeropickApplication.java
@@ -1,7 +1,11 @@
 package kw.zeropick;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "https://ec2-15-164-252-103.ap-northeast-2.compute.amazonaws.com")})
 
 @SpringBootApplication
 public class ZeropickApplication {
