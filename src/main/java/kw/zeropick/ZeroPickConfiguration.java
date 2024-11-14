7c8a2bd6e28ee0bfd7394ed5d00791dcc336510b
Suhun0331 <rlatngns6046@naver.com>
2024-11-14 18:40:45 +0900

    Feat : Add configuration4

--- src/main/java/kw/zeropick/ZeroPickConfiguration.java
+++ src/main/java/kw/zeropick/ZeroPickConfiguration.java
@@ -10,8 +10,7 @@ public class ZeroPickConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000",
-                        "http://ec2-15-164-252-103.ap-northeast-2.compute.amazonaws.com:8080")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
