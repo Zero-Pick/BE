c97736b426cef355501cf12362bc2065653648e3
Suhun0331 <rlatngns6046@naver.com>
2024-11-14 19:03:05 +0900

    Feat : Add configuration8

--- src/main/java/kw/zeropick/ZeroPickConfiguration.java
+++ src/main/java/kw/zeropick/ZeroPickConfiguration.java
@@ -1,8 +1,13 @@
 package kw.zeropick;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@OpenAPIDefinition(servers = {@Server(url = "http://ec2-15-164-252-103.ap-northeast-2.compute.amazonaws.com:8080",
+        description = "Default Server URL")})
 
 @Configuration
 public class ZeroPickConfiguration implements WebMvcConfigurer {
