6013ac6cb43d9c0f0c619b7baec07d2ed9d449b7
Suhun0331 <rlatngns6046@naver.com>
2025-01-02 12:23:42 +0900

    Feat : update & add category enum type

--- src/main/java/kw/zeropick/product/domain/Category.java
+++ src/main/java/kw/zeropick/product/domain/Category.java
@@ -1,6 +1,12 @@
 package kw.zeropick.product.domain;
 
 public enum Category {
-    Test
-
+    BEVERAGE_TEA,         // 음료, 차
+    SNACK_BREAD,          // 과자, 빵
+    DAIRY_PRODUCTS,       // 유가공품
+    ICE_CREAM,            // 아이스크림, 빙과
+    CHOCOLATE,            // 초콜릿
+    NOODLE_RICE_CAKE,     // 면, 떡
+    AGRICULTURAL_PRODUCTS,// 농수산가공식품
+    ETC                   // 기타
 }
